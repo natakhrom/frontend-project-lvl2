0aaad13893bd4263a83fc9a823c8ae6e80c8063b
Natalia <natakhrom2701@yandex.ru>
2022-04-18 12:00:52 +0200

    add formatter json

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { test, expect } from '@jest/globals';
+import { readFileSync } from 'fs';
 import genDiff from '../src/genDiff.js';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -13,105 +14,9 @@ const received1 = genDiff(filepath1, filepath2, 'stylish');
 const received2 = genDiff(filepath1, filepath2, 'plain');
 const received3 = genDiff(filepath1, filepath2, 'json');
 
-const expected1 = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
-const expected2 = `Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`;
-const expected3 = `{
-    "common": {
-      "+ follow": false,
-        "setting1": "Value 1",
-      "- setting2": 200,
-      "- setting3": true,
-      "+ setting3": null,
-      "+ setting4": "blah blah",
-      "+ setting5": {
-            "key5": "value5"
-        },
-        "setting6": {
-            "doge": {
-              "- wow": "",
-              "+ wow": "so much"
-            },
-            "key": "value",
-          "+ ops": "vops"
-        }
-    },
-    "group1": {
-      "- baz": "bas",
-      "+ baz": "bars",
-        "foo": "bar",
-      "- nest": {
-            "key": "value"
-        },
-      "+ nest": "str"
-    },
-  "- group2": {
-        "abc": 12345,
-        "deep": {
-            "id": 45
-        }
-    },
-  "+ group3": {
-        "deep": {
-            "id": {
-                "number": 45
-            }
-        },
-        "fee": 100500
-    }
-}`;
+const expected1 = readFileSync(getFixturePath('stylishFormat.txt'), 'utf-8');
+const expected2 = readFileSync(getFixturePath('plainFormat.txt'), 'utf-8');
+const expected3 = readFileSync(getFixturePath('jsonFormat.txt'), 'utf-8');
 
 test('get difference', () => {
   expect(received1).toEqual(expected1);
--- formatters/index.js
+++ formatters/index.js
@@ -1,12 +1,11 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-import json from './json.js';
 
 const formatter = (data, format) => {
   switch (format) {
     case 'stylish': return stylish(data);
     case 'plain': return plain(data);
-    case 'json': return json(data);
+    case 'json': return JSON.stringify(data, null, '  ');
     default: throw new Error(`Unknown formatName: '${format}'!`);
   }
 };
